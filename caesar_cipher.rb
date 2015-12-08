c6d68816621d5f0b07ae6decd61e3d804cd5341f
Brett Peters <brett.peters1@gmail.com>
2015-12-08 00:07:18 -0700

    Fix regex

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,6 @@
 def caesar_cipher(message, rotation)
   cipher = Hash[("a".."z").zip(("a".."z").to_a.rotate(rotation)) +
                 ("A".."Z").zip(("A".."Z").to_a.rotate(rotation))]
-  message.gsub(/[A-z]/, cipher) 
+  message.gsub(/[A-Za-z]/, cipher) 
 end
 
